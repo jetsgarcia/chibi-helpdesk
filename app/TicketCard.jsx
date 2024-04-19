73315af10e828068bff5c9deed1ba86cbbcfc290
Jetter <jlgarcia2@student.apc.edu.ph>
2024-04-19 19:02:34 +0800

    Added ticket card component

---
+++ app/TicketCard.jsx
@@ -0,0 +1,29 @@
+export default function TicketCard({ title, description, priorityLevel }) {
+  let priorityClassValue;
+  if (priorityLevel === "low") {
+    priorityClassValue = "bg-green-100 text-green-800";
+  } else if (priorityLevel === "medium") {
+    priorityClassValue = "bg-yellow-100 text-yellow-800";
+  } else if (priorityLevel === "high") {
+    priorityClassValue = "bg-red-100 text-red-800";
+  }
+
+  return (
+    <div className="flex justify-between bg-white p-4 rounded-md items-center">
+      <div>
+        <h2 className="font-semibold">{title}</h2>
+        <p className="text-sm">{description}</p>
+      </div>
+      <div className={priorityClassValue + " px-2 py-1 rounded-md"}>
+        {
+          /* For capitalizing the first letter */
+          priorityLevel.charAt(0).toUpperCase() +
+            priorityLevel.slice(1) +
+            // For spacing between the priority level and the word "Priority"
+            " "
+        }
+        Priority
+      </div>
+    </div>
+  );
+}
