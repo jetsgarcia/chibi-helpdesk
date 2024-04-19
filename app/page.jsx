a19588d5fcf4e4d4d0cc02c04b3bbd6913f1ae0f
Jetter <jlgarcia2@student.apc.edu.ph>
2024-04-19 09:17:32 +0800

    Refactored the layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,18 @@
 import PageTitle from "./PageTitle";
 import AddTicketButton from "./AddTicketButton";
+import TicketCard from "./TicketCard";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-between m-auto w-3/5 pt-4">
-        <PageTitle />
-        <AddTicketButton />
+      <div className="m-auto w-3/5 pt-4">
+        {/* Page Headers */}
+        <div className="flex justify-between">
+          <PageTitle />
+          <AddTicketButton />
+        </div>
+        {/* Ticket list */}
+        <div></div>
       </div>
     </main>
   );
