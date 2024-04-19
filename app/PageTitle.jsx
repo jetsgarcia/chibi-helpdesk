08f39da0c49b98bd56337be6a631bdddae03d94d
Jetter <jlgarcia2@student.apc.edu.ph>
2024-04-19 08:45:24 +0800

    Added headers for home page

---
+++ app/PageTitle.jsx
@@ -0,0 +1,8 @@
+export default function PageTitle() {
+  return (
+    <div>
+      <div className="text-primary text-xl font-bold">Tickets</div>
+      <h1>Currently opened tickets</h1>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,13 @@
+import PageTitle from "./PageTitle";
+import AddTicketButton from "./AddTicketButton";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="flex justify-between m-auto w-3/5 pt-4">
+        <PageTitle />
+        <AddTicketButton />
+      </div>
+    </main>
+  );
 }
