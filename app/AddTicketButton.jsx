3f93d838340599dcb29f77cdc92ea706b21146bb
Jetter <jlgarcia2@student.apc.edu.ph>
2024-04-23 11:37:49 +0800

    Made the button interactive

--- app/AddTicketButton.jsx
+++ app/AddTicketButton.jsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
+// TODO: Implement the AddTicketButton component to add a ticket in backend
 export default function AddTicketButton() {
-  return <Button>Add Ticket</Button>;
+  return (
+    <Link href="/new-ticket">
+      <Button>Add Ticket</Button>
+    </Link>
+  );
 }
