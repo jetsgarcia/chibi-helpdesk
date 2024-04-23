25b29ef5732c0c32675b59228658160e2cd67f68
Jetter <jlgarcia2@student.apc.edu.ph>
2024-04-23 11:38:09 +0800

    Removed unnecesary comment

--- app/AddTicketButton.jsx
+++ app/AddTicketButton.jsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-// TODO: Implement the AddTicketButton component to add a ticket in backend
 export default function AddTicketButton() {
   return (
     <Link href="/new-ticket">
